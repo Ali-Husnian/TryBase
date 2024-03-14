1e63c49f5b1653d0ba25e611d7903e60a9ea3474
ali husnain <pkihusnain220@gmail.com>
2024-03-14 12:44:00 +0500

    Preparing form deployment

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -65,7 +67,8 @@ app.use(
         ],
     }),
 );
-
+app.use(compression());
+app.use(cors());
 // Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
